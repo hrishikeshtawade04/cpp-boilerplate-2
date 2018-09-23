be4e5d000273b857579b7e4a261b52a39b6d699c
sauravkdeo <you@example.com>
2018-09-23 03:31:22 -0400

    added two unit tests to test setParameters function and compute function

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,26 @@
 #include <gtest/gtest.h>
+#include "PIDController.h"
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+
+/**
+ * Test class initialization
+ */
+TEST(PIDControllerTest,parameterSetting) {
+  PIDController instancePID;
+  instancePID.setParamters(1.0, 2.0, 3.0);
+
+  EXPECT_EQ(1.0, instancePID.getKp());
+  EXPECT_EQ(2.0, instancePID.getKi());
+  EXPECT_EQ(3.0, instancePID.getKd());
+}
+
+/**
+ * Test compute method
+ */
+TEST(PIDControllerTest, computeFunctionOutput) {
+
+  PIDController instancePID;
+  instancePID.setParamters(1.0, 2.0, 3.0);
+
+  EXPECT_EQ(10.0, instancePID.compute(10.0, 1.0,2));
 }
