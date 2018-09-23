3193b01521b418b96d6c49daf601bbabc3e9696a
sauravkdeo <you@example.com>
2018-09-23 00:38:32 -0400

    Implemented PIDController class function

---
+++ app/main2.cpp
@@ -0,0 +1,51 @@
+
+    /** @file main2.cpp
+     *  @copyright Copyright 2018
+     *  @brief      *
+     *  Defines  a class PidController with kp,ki and kd as private member and
+     *  compute and setParamters as methods.
+     *
+     *  It takes target setpoint and actual velocity as two inputs and run the method compute which return a value 10.0
+     *
+     *  @author Saurav Kumar
+     *  @author Hrikesh Tawade
+     */
+#include <iostream>
+#include "../include/PIDController.h"
+
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+///constructor for class PIDController.
+PIDController::PIDController() {
+  kp = 0;ki=0;kd=0;
+}
+/// destructor for class PIDController.
+PIDController::~PIDController() {
+}
+///
+/// @param targetSetpoint
+/// @param actualVelocity
+/// @param int
+/// @return
+double PIDController::compute(float targetSetpoint, float actualVelocity,int iteration)//!< a member function which returns constant 10.0
+  {
+  return 10.0;
+  }
+
+  /**
+    *
+    * @param kp
+    * @param ki
+    * @param kd
+    * @return null
+    */
+  void PIDController::setParamters(float kp, float ki,float kd)//!< a member function.
+    {
+     this->kp=kp;
+     this->ki=ki;
+     this->kd=kd;
+
+    }
